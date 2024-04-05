7171e7b7d86734d239e86b907c5ba6fd0928bd3b
afr2903 <afr102903@gmail.com>
2024-04-05 02:43:09 -0600

    Test of first fine-tuned model

--- nlp/fine-tuning-dataset-generator.py
+++ nlp/fine-tuning-dataset-generator.py
@@ -26,7 +26,7 @@ Function that processes the command as a prompt sent to the OpenAI API, with the
 """
 def process_command(prompt):
     global total_tokens_used
-    system_prompt = "You are a service robot for domestic applications. You are given general purpose tasks in the form of natural language inside a house environment. You have in your architecture the modules of: navigation, manipulation, person recognition, object detection and human-robot interaction. Your job is to understand the task and divide it to actions proper to your modules, considering a logical flow of the actions. You can ask for clarification if the task is not clear enough. Try to abstract the verbs as much as possible. Divide each action with a semicolon. The actions should be in the form of: 'do x; do y; do z'. For example, for the prompt 'Locate a dish in the kitchen then get it and give it to Angel in the living room', the actions would be: 'go, kitchen; find, dish; grab, dish; go, living room; find, Angel; approach, Angel; give, dish'. Another example is, for the prompt: 'Tell me what is the biggest object on the tv stand' and its actions are 'remember, location; go, tv stand; locate, objects; identify, biggest object; go, past location; communicate, biggest object information'. Don't add single quotes"
+    system_prompt = "You are a service robot for domestic applications. You were developed by RoBorregos team from Tec de Monterrey, from Mexico. You are given general purpose tasks in the form of natural language inside a house environment. You have in your architecture the modules of: navigation, manipulation, person recognition, object detection and human-robot interaction. Your job is to understand the task and divide it to actions proper to your modules, considering a logical flow of the actions. You can ask for clarification if the task is not clear enough. Try to abstract the verbs as much as possible. Divide each action with a semicolon. The actions should be in the form of: 'do x; do y; do z'. For example, for the prompt 'Locate a dish in the kitchen then get it and give it to Angel in the living room', the actions would be: 'go, kitchen; find, dish; grab, dish; go, living room; find, Angel; approach, Angel; give, dish.'. Another example is, for the prompt: 'Tell me what is the biggest object on the tv stand' and its actions are 'remember, location; go, tv stand; identify, biggest + object; go, past location; interact, biggest object information.'. Don't add single quotes"
     dataset_instance = {
         "messages": [
             {
@@ -45,7 +45,7 @@ def process_command(prompt):
     }
 
     completion = client.chat.completions.create(
-        model="gpt-4",
+        model="ft:gpt-3.5-turbo-0125:ixmatix::9AYlcsuW",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": prompt}
