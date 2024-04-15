a689379309c25c3db2a3237da41c5be041d100c6
Oscar-gg <oscar.arreola.jr@gmail.com>
2024-04-14 22:14:20 -0600

    modified kws sensitivities and respeaker bug

--- ws/src/speech/scripts/KWS.py
+++ ws/src/speech/scripts/KWS.py
@@ -36,9 +36,8 @@ class KWS(object):
         # Sensitivities for detecting keywords. Each value should be a number within [0, 1]. 
         # A higher sensitivity results in fewer misses at the cost of increasing the false alarm rate. If not set 0.5 will be used.
         keyword_paths = list_files_with_extension(KEYWORD_DIR, '.ppn')
-        sensitivities = [0.3] * len(keyword_paths)
-        
-        # print(sensitivities)
+        sensitivities = [0.8] * len(keyword_paths)
+
         access_key = ACCESS_KEY
         try:
             self.porcupine = pvporcupine.create(
