37224bc95e56d97c198a6f2aa8898714b56765bc
Oscar-gg <oscar.arreola.jr@gmail.com>
2024-04-16 10:06:07 -0600

    modified test speaker file

--- ws/src/speech/scripts/TestSpeaker.py
+++ ws/src/speech/scripts/TestSpeaker.py
@@ -2,6 +2,8 @@
 # python3 -m sounddevice
 
 from WavUtils import WavUtils
+from SpeechApiUtils import SpeechApiUtils
+import os
 
 import sounddevice as sd
 def get_devices():
@@ -16,11 +18,16 @@ def get_devices():
 
 if __name__ == "__main__":
     mp3_path = "play.mp3"
-    # WavUtils.play_mp3(mp3_path, device_index=11)
+    WavUtils.play_mp3(mp3_path, device_index=0)
     # wav_path = "6_recorded_audio.wav"
     # wav_path = "test.wav"
     # WavUtils.play_mp3(mp3_path, device_index=12)
     # WavUtils.play_wav(wav_path, device_index=4)
-    get_devices()
+    SPEAKER_DEVICE_NAME = os.getenv("SPEAKER_DEVICE_NAME", default=None)
+    SPEAKER_INPUT_CHANNELS = int(os.getenv("SPEAKER_INPUT_CHANNELS", default=2))
+    SPEAKER_OUT_CHANNELS = int(os.getenv("SPEAKER_OUT_CHANNELS", default=0))
 
-    
+    OUTPUT_DEVICE_INDEX = SpeechApiUtils.getIndexByNameAndChannels(SPEAKER_DEVICE_NAME, SPEAKER_INPUT_CHANNELS, SPEAKER_OUT_CHANNELS)
+
+    get_devices()
+    print("OUTPUT_DEVICE_INDEX =", OUTPUT_DEVICE_INDEX)
