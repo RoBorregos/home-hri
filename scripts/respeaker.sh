7c6a0e18a972453be6cda4076922f100f39639cb
Oscar-gg <oscar.arreola.jr@gmail.com>
2024-04-16 09:28:21 -0600

    fix: dockerfile .bashrc

--- scripts/respeaker.sh
+++ scripts/respeaker.sh
@@ -1,4 +1,6 @@
 # Script to set up permissions for ReSpeaker
+# Note: if the usb is detected in another subsystem that isn't usb, the following command could be executed
+# sudo echo 'ATTRS{idVendor}=="2886", MODE="0666"' | sudo tee -a /etc/udev/rules.d/99-usb-permissions.rules
 
 # Allow usb access to respeaker without sudo in python script
 sudo echo '"SUBSYSTEM=="usb", ATTRS{idVendor}=="2886", MODE="0666"' | sudo tee -a /etc/udev/rules.d/99-usb-permissions.rules
