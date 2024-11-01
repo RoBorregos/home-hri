8c7e6a2df9d05fbbd422bcb85ef51e2140e6c9ea
Oscar Arreola <oscar.arreola.jr@gmail.com>
2024-11-01 06:39:40 -0600

    refactor: formatting

--- ws/src/frida_language_processing/scripts/item_categorization.py
+++ ws/src/frida_language_processing/scripts/item_categorization.py
@@ -15,23 +15,28 @@ from openai import OpenAI
 
 from frida_hri_interfaces.srv import ItemsCategory, ItemsCategoryResponse
 
-SIMILARITY_THRESHOLD = 0.7 # Minimum similarity percentage in which 2 words are considered the same
+# Minimum similarity percentage in which 2 words are considered the same
+SIMILARITY_THRESHOLD = 0.7
 
 rp = rospkg.RosPack()
-DATAFRAMES_DIR = rp.get_path('frida_language_processing') + "/scripts/dataframes/"
+DATAFRAMES_DIR = rp.get_path(
+    'frida_language_processing') + "/scripts/dataframes/"
 ITEMS_EXTRACT_TOPIC = "items_category"
 OBJECT_EXTRACT_TOPIC = "object_category"
 
 ITEM_EXTRACT_TOPIC = "known_item"
 
+
 class ItemCategorization:
     """class to handle both services for item categorization"""
+
     def __init__(self) -> None:
         """Initialize the ROS node"""
         self._node = rospy.init_node("item_categorization")
         self._rate = rospy.Rate(10)
         # rospy.Service(ITEMS_EXTRACT_TOPIC, ItemsCategory, self.extract_category)
-        rospy.Service(ITEMS_EXTRACT_TOPIC, ItemsCategory, self.get_local_embedding)
+        rospy.Service(ITEMS_EXTRACT_TOPIC, ItemsCategory,
+                      self.get_local_embedding)
         rospy.Service(OBJECT_EXTRACT_TOPIC, ItemsCategory, self.extract_item)
         self.openai_client = OpenAI(
             api_key=os.getenv("OPENAI_API_KEY")
@@ -45,14 +50,15 @@ class ItemCategorization:
             self.category_embeddings = pickle.load(f)
 
         rospy.spin()
-    
+
     def extract_item(self, req):
         """Service to return the most representative category of the items given"""
         target_item = req.items[0]
         detected_items = req.items[1:]
 
         item_embedding = self.get_embedding(target_item)
-        possible_items = self.get_similar_items(item_embedding, self.items_dataframe)
+        possible_items = self.get_similar_items(
+            item_embedding, self.items_dataframe)
 
         response = ItemsCategoryResponse()
 
@@ -60,7 +66,7 @@ class ItemCategorization:
             if possible_items.iloc[i]['name'] in detected_items:
                 response.category = possible_items.iloc[i]['name']
                 break
-        
+
         return response
 
     def extract_category(self, req):
@@ -69,7 +75,8 @@ class ItemCategorization:
         categories = []
         for item in items:
             item_embedding = self.get_embedding(item)
-            item_category = self.get_category_similarity(item_embedding, self.items_dataframe)
+            item_category = self.get_category_similarity(
+                item_embedding, self.items_dataframe)
             categories.append(item_category)
 
         representative_category = max(set(categories), key=categories.count)
@@ -82,10 +89,11 @@ class ItemCategorization:
         target_item = req.items[0]
         item_embedding = self.model.encode(target_item, convert_to_tensor=True)
         for category_id, category_data in self.category_embeddings.items():
-             if category_data is not None:
-                category_data['similarity'] = self.get_embeddings_similarity(item_embedding, category_data['category_embedding'])
+            if category_data is not None:
+                category_data['similarity'] = self.get_embeddings_similarity(
+                    item_embedding, category_data['category_embedding'])
                 self.category_embeddings[category_id] = category_data
-        
+
         possible_item = None
 
         for category_id, category_data in self.category_embeddings.items():
@@ -99,31 +107,35 @@ class ItemCategorization:
 
     def get_embedding(self, text):
         """Get the embedding of a given text"""
-        return self.openai_client.embeddings.create(input = [text], model="text-embedding-3-small").data[0].embedding
+        return self.openai_client.embeddings.create(input=[text], model="text-embedding-3-small").data[0].embedding
 
     def get_local_embedding(self, object_embedding, category_embedding):
         return util.pytorch_cos_sim(object_embedding, category_embedding).item()
-    
+
     def get_embeddings_similarity(self, object_embedding, category_embedding):
         return util.pytorch_cos_sim(object_embedding, category_embedding).item()
 
     def get_similar_items(self, input, dataframe):
         """Get the category of the item based on the similarity of the embedding"""
         # Get name similarity
-        dataframe['similarity'] = dataframe['name_embedding'].apply(lambda x: np.dot(x, input))
+        dataframe['similarity'] = dataframe['name_embedding'].apply(
+            lambda x: np.dot(x, input))
         sorted_names = dataframe.sort_values('similarity', ascending=False)
         return sorted_names
 
     def get_category_similarity(self, input, dataframe):
         """Get the category of the item based on the similarity of the embedding"""
         # Get name similarity
-        dataframe['similarity'] = dataframe['name_embedding'].apply(lambda x: np.dot(x, input))
+        dataframe['similarity'] = dataframe['name_embedding'].apply(
+            lambda x: np.dot(x, input))
         sorted_names = dataframe.sort_values('similarity', ascending=False)
         best_name_similarity = sorted_names.iloc[0]['similarity']
 
-        # Get category similarity 
-        dataframe['similarity'] = dataframe['category_embedding'].apply(lambda x: np.dot(x, input))
-        sorted_categories = dataframe.sort_values('similarity', ascending=False)
+        # Get category similarity
+        dataframe['similarity'] = dataframe['category_embedding'].apply(
+            lambda x: np.dot(x, input))
+        sorted_categories = dataframe.sort_values(
+            'similarity', ascending=False)
         best_category_similarity = sorted_categories.iloc[0]['similarity']
 
         # Return the category with the highest similarity, comparing name and category
@@ -131,6 +143,7 @@ class ItemCategorization:
             return sorted_names.iloc[0]['category']
         else:
             return sorted_categories.iloc[0]['category']
+
 
 if __name__ == "__main__":
     try:
--- ws/src/speech/scripts/Say.py
+++ ws/src/speech/scripts/Say.py
@@ -30,7 +30,8 @@ SPEAKER_DEVICE_NAME = os.getenv("SPEAKER_DEVICE_NAME", default=None)
 SPEAKER_INPUT_CHANNELS = int(os.getenv("SPEAKER_INPUT_CHANNELS", default=2))
 SPEAKER_OUT_CHANNELS = int(os.getenv("SPEAKER_OUT_CHANNELS", default=0))
 
-OUTPUT_DEVICE_INDEX = SpeechApiUtils.getIndexByNameAndChannels(SPEAKER_DEVICE_NAME, SPEAKER_INPUT_CHANNELS, SPEAKER_OUT_CHANNELS)
+OUTPUT_DEVICE_INDEX = SpeechApiUtils.getIndexByNameAndChannels(
+    SPEAKER_DEVICE_NAME, SPEAKER_INPUT_CHANNELS, SPEAKER_OUT_CHANNELS)
 
 if OUTPUT_DEVICE_INDEX is None:
     print("Warning: output device index not found, using system default.")
@@ -38,18 +39,20 @@ if OUTPUT_DEVICE_INDEX is None:
 DEBUG = True
 OFFLINE = True
 
+
 class Say(object):
 
     def __init__(self):
         self.connected = False
         if not OFFLINE:
             self.connected = self.is_connected()
-        
+
         rospy.Service(SPEAK_TOPIC, Speak, self.speak_handler)
 
-        self.text_suscriber = rospy.Subscriber(SPEAK_NOW_TOPIC, String, self.callback)
+        self.text_suscriber = rospy.Subscriber(
+            SPEAK_NOW_TOPIC, String, self.callback)
         self.hear_publisher = rospy.Publisher("saying", Bool, queue_size=20)
-    
+
     @staticmethod
     def is_connected():
         '''
@@ -71,7 +74,7 @@ class Say(object):
             pass
         rospy.logerr("No internet connection.")
         return False
-    
+
     def play_audio(self, file_path):
         mixer.pre_init(frequency=48000, buffer=2048)
         mixer.init()
@@ -79,18 +82,19 @@ class Say(object):
         mixer.music.play()
         while mixer.music.get_busy():
             pass
-    
+
     """
     New implementation of speak as a service
     """
+
     def speak_handler(self, req):
         self.debug("I will say: " + req.text)
         return self.trySay(req.text)
 
     def debug(self, text):
-        if(DEBUG):
+        if (DEBUG):
             self.log(text)
-            
+
     def log(self, text):
         rospy.loginfo(text)
 
@@ -98,13 +102,13 @@ class Say(object):
         self.debug("I will say: " + msg.data)
         self.trySay(msg.data)
 
-    def connectedVoice(self, text): 
+    def connectedVoice(self, text):
         tts = gTTS(text=text, lang='en')
         save_path = "play.mp3"
         tts.save(save_path)
         self.debug("Saying...")
-        #WavUtils.play_mp3(save_path, device_index=OUTPUT_DEVICE_INDEX)
-        #.play_mp3(save_path, device_index=OUTPUT_DEVICE_INDEX)
+        # WavUtils.play_mp3(save_path, device_index=OUTPUT_DEVICE_INDEX)
+        # .play_mp3(save_path, device_index=OUTPUT_DEVICE_INDEX)
         self.play_audio(save_path)
         self.debug("Finished speaking.")
 
@@ -115,7 +119,7 @@ class Say(object):
             if OFFLINE or not self.connected:
                 self.offlineVoice(text)
             else:
-                self.connectedVoice(text)  
+                self.connectedVoice(text)
             success = True
         except Exception as e:
             print(e)
@@ -128,25 +132,24 @@ class Say(object):
         rospy.logwarn("Published: True ")
         return success
 
-
     def offlineVoice(self, text):
         output_base_path = os.path.join(CURRENT_DIRECTORY, "offline_voice")
-        
+
         text_chunks = self.split_text(text, 4000)
         counter = 0
-        
+
         # Generate all wav files for each chunk
         for chunk in text_chunks:
             counter += 1
             output_path = os.path.join(output_base_path, f"{counter}.wav")
             self.synthesize_voice_offline(output_path, chunk)
-        
+
         print(f"Generated {counter} wav files.")
-        
+
         # Play and remove all mp3 files
         for i in range(1, counter+1):
             save_path = os.path.join(output_base_path, f"{i}.wav")
-            #WavUtils.play_wav(save_path, device_index=OUTPUT_DEVICE_INDEX)
+            # WavUtils.play_wav(save_path, device_index=OUTPUT_DEVICE_INDEX)
             self.play_audio(save_path)
             sleep(0.5)
             WavUtils.discard_wav(save_path)
@@ -167,28 +170,28 @@ class Say(object):
     def synthesize_voice_offline(output_path, text):
         """Synthesize text using the offline voice model."""
         model_path = os.path.join(CURRENT_DIRECTORY, 'offline_voice', MODEL)
-        
+
         command = [
             'echo', f'"{text}"', '|',
             'python3.10', '-m', 'piper', '--model', model_path, '--output_file', output_path
         ]
-        
+
         subprocess.run(' '.join(command), shell=True)
-        
 
 
 def main():
     rospy.init_node('say', anonymous=True)
     global DEBUG
     DEBUG = rospy.get_param('~debug', False)
-    
+
     global OFFLINE
     OFFLINE = rospy.get_param('~offline', False)
-    
+
     say = Say()
     say.log('Say Module Initialized.')
-    
+
     rospy.spin()
+
 
 if __name__ == '__main__':
     main()
