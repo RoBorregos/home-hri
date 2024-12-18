2bf74e9ae70b2326b362da89a50f5b5a673a5aa5
lleo-13 <a00830708@tec.mx>
2024-12-18 11:25:35 -0600

    "minor changes local embeddings node"

--- ws/src/frida_language_processing/scripts/item_categorization.py
+++ ws/src/frida_language_processing/scripts/item_categorization.py
@@ -3,18 +3,14 @@
 ROS2 node that returns the best match of an item or list of items
 """
 import rclpy
-import pandas as pd
 import numpy as np
 import os
 import pickle
 from sentence_transformers import SentenceTransformer, util
-import yaml
-import tqdm
-import time
-import pynvml
 from frida_hri_interfaces.srv import ItemsCategory
 from ament_index_python.packages import get_package_share_directory
-import pynvml
+
+
 DATAFRAMES_DIR = os.path.join(get_package_share_directory('frida_language_processing'), 'scripts/dataframes/')
 # Minimum similarity percentage in which 2 words are considered the same
 SIMILARITY_THRESHOLD = 0.7
@@ -35,45 +31,51 @@ class ItemCategorization:
         self._node.create_service(ItemsCategory, OBJECT_EXTRACT_TOPIC , self.extract_item)
         #load the model
         self.model = SentenceTransformer("all-MiniLM-L12-v2")
-        current_dir = os.path.dirname(os.path.abspath(__file__))
+        current_dir = "/home-hri/nlp/Embeddings analysis/dataframes"
         #Load the embeddings already calculated/ embeddings database
-        with open(os.path.join(current_dir, "assets", "embeddings_items.pkl"), "rb") as f:
+        with open(os.path.join(current_dir, "embeddings_items.pkl"), "rb") as f:
             self.items_dataframe = pickle.load(f)
 
-        with open(os.path.join(current_dir, "assets", "embeddings_categories.pkl"), "rb") as f:
+        with open(os.path.join(current_dir, "embeddings_categories.pkl"), "rb") as f:
             self.category_embeddings = pickle.load(f)
         
-        with open(os.path.join(current_dir, "assets", "embeddings_locations.pkl"), "rb") as f:
+        with open(os.path.join(current_dir, "embeddings_locations.pkl"), "rb") as f:
             self.locations_embeddings = pickle.load(f)
         
-        with open(os.path.join(current_dir, "assets", "embeddings_actions.pkl"), "rb") as f:
+        with open(os.path.join(current_dir, "embeddings_actions.pkl"), "rb") as f:
             self.actions_embeddings = pickle.load(f)
             #spin the node
         rclpy.spin(self._node)
 
     def extract_item(self, req):
         """Service to return the best match of the items given"""
-        start_time = time.time()
         response = ItemsCategory.Response()
         i = 0
-        for item in req.items:
-            item_embedding = [self.get_local_embedding(item)]
-            possible_items = self.get_similar_items(item_embedding[0], self.items_dataframe)
-            response.matches[i] = possible_items.iloc[0]['name']
-            i = 0 + 1
-        response.gpu_usage = self.get_gpu_usage()
-        response.time = time.time() - start_time
+        if req.category == "items":
+            for item in req.items:
+                item_embedding = [self.get_local_embedding(item)]
+                possible_items = self.get_similar_items(item_embedding[0], self.items_dataframe)
+                response.matches[i] = possible_items.iloc[0]['name']
+                i = i + 1
+        elif req.category == "locations":
+            for location in req.items:
+                location_embedding = [self.get_local_embedding(location)]
+                possible_locations = self.get_similar_items(location_embedding[0], self.locations_embeddings)
+                response.matches[i] = possible_locations.iloc[0]['name']
+                i = i + 1
+        elif req.category == "actions":
+            for action in req.items:
+                action_embedding = [self.get_local_embedding(action)]
+                possible_actions = self.get_similar_items(action_embedding[0], self.actions_embeddings)
+                response.matches[i] = possible_actions.iloc[0]['name']
+                i = i + 1
+        elif req.category == "category":
+            for category in req.items:
+                category_embedding = [self.get_local_embedding(category)]
+                possible_categories = self.get_similar_items(category_embedding[0], self.category_embeddings)
+                response.matches[i] = possible_categories.iloc[0]['name']
+                i = i + 1
         return response
-
-
-    def get_gpu_usage():
-        pynvml.nvmlInit()
-        handle = pynvml.nvmlDeviceGetHandleByIndex(0)  # Assuming single GPU
-        utilization = pynvml.nvmlDeviceGetUtilizationRates(handle)
-        memory_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
-        memory_allocated = memory_info.used / 1024**2  # Convert to MB
-        total_memory = memory_info.total / 1024**2  # Convert to MB
-        return [memory_allocated, total_memory, utilization.gpu]
 
     def get_local_embedding(self, object):
         embedding = self.model.encode(object, convert_to_tensor=True)
